7148f318e9a31d0083b693c67ec6281319c82d5b
Yashaswi Gusain <gusainyashaswi@gmail.com>
2026-07-16 13:50:06 +0530

    Update package dependencies and enhance AST import extraction functionality

--- backend/src/utils/ast.utils.js
+++ backend/src/utils/ast.utils.js
@@ -1,3 +1,4 @@
+const traverse = require("@babel/traverse").default;
 
 function extractImports(ast) {
 
@@ -29,7 +30,35 @@ function isInternalImport(importPath) {
            importPath.startsWith("../");
 }
 
+function extractImports(ast) {
+
+    const imports = [];
+
+    traverse(ast, {
+
+        ImportDeclaration(path) {
+            imports.push(
+                path.node.source.value
+            );
+        },
+
+        CallExpression(path) {
+            
+            if (path.node.callee.name !== "require"){
+                return;
+            }
+
+            imports.push(
+                path.node.arguments[0].value
+            );
+        }
+    });
+
+    return imports;
+}
+
 module.exports = {
     extractImports,
-    isInternalImport
+    isInternalImport,
+    extractExports
 }
