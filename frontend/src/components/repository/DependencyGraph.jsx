9edc4f35050f3fc2ba3d137d5f41a2cedae608fb
Yashaswi Gusain <gusainyashaswi@gmail.com>
2026-07-23 12:58:21 +0530

    feat: enhance DependencyGraph component to manage selected file state and update node styling based on selection

--- frontend/src/components/repository/DependencyGraph.jsx
+++ frontend/src/components/repository/DependencyGraph.jsx
@@ -4,28 +4,53 @@ import "@xyflow/react/dist/style.css";
 import { useRepository } from "../../context/RepositoryContext";
 
 function DependencyGraph() {
-    const { repository } = useRepository();
+    const { repository, selectedFile, setSelectedFile } = useRepository();
 
-    const nodes = repository.dependencyGraph.nodes.map((node, index) => ({
-    id: node.id,
+    const nodes = repository.dependencyGraph.nodes.map((node, index) => {
+    const isSelected = selectedFile && selectedFile.path === node.id;
 
-    position: {
-        x: 100,
-        y: index * 150,
-    },
+    return {
+        id: node.id,
 
-    data: {
-        label: node.label,
-    },
+        position: {
+            x: 100,
+            y: index * 150,
+        },
 
-    type: "default",
-}));
+        data: {
+            label: node.label,
+        },
+
+        type: "default",
+
+        style: {
+            border: isSelected
+                ? "3px solid #2563eb"
+                : "1px solid #555",
+
+            background: isSelected
+                ? "#dbeafe"
+                : "white",
+        },
+    };
+});
+
 
 const edges = repository.dependencyGraph.edges;
 
+function handleNodeClick(event, node) {
+    const file = repository.files.find(
+        (file) => file.path === node.id
+    );
+
+    if (file) {
+        setSelectedFile(file);
+    }
+}
+
     return(
         <div className="border rounded-lg overflow-hidden" style={{ height: "75vh" }}>
-            <ReactFlow nodes={nodes} edges={edges} fitView/>
+            <ReactFlow nodes={nodes} edges={edges} fitView onNodeClick={handleNodeClick}/>
         </div>
     )
 }
